e8370f3c910c206415b39063544660c36d32721d
Philip Le <lephil505006@gmail.com>
2023-12-01 14:31:04 -0600

    Creating a card with button

--- components/Card.js
+++ components/Card.js
@@ -1,11 +1,26 @@
-// components/Card.js
-import React from 'react';
+import React, { useState } from 'react';
 
-const Card = ({ title, description }) => {
+const Card = ({ title, description, imageUrl }) => {
+  const [showMore, setShowMore] = useState(false);
+
+  const toggleShowMore = () => {
+    setShowMore(!showMore);
+  };
+
   return (
     <div className="card">
       <h2>{title}</h2>
+      {imageUrl && <img src={imageUrl} alt={title} className="card-image" />}
       <p>{description}</p>
+      <button onClick={toggleShowMore}>
+        {showMore ? 'View Less' : 'View More'}
+      </button>
+      {showMore && (
+        <p>
+          Additional information or content you want to display when "View More"
+          is clicked.
+        </p>
+      )}
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,9 @@ const Home = () => {
     <div className="container">
       <h1>Welcome to My Website</h1>
       <Card
-        title="Card Title"
-        description="This is a sample card with some information."
+        title="Illinois Institute of Technology"
+        description="Master's Degree"
+        imageUrl="https://cdn.britannica.com/13/149313-050-E9E1EC08/SR-Crown-Hall-Illinois-Institute-of-Technology-1956.jpg"
       />
     </div>
   );
