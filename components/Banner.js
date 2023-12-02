6fd91a0f38f756b87a6ec66c7811cf3f9b82fdb9
Philip Le <lephil505006@gmail.com>
2023-12-01 18:57:13 -0600

    Setting a third card

--- components/Banner.js
+++ components/Banner.js
@@ -4,7 +4,12 @@ const Banner = () => {
   return (
     <div className="banner">
       <h1>Philip Le Resume</h1>
-      <p>Resume Info</p>
+      <p>
+        <b>Phone Number</b> ### ### ####
+      </p>
+      <p>
+        <b>Email Address</b> ple2@hawk.iit.edu
+      </p>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,20 @@ import Banner from '../components/Banner';
 const Home = () => {
   const cardsData = [
     {
-      title: 'Illinois Institute of Technology',
+      title: 'Illinois Institute of Technology - Web and App Development',
       caption: "Master's Degree",
       imageUrl:
         'https://cdn.britannica.com/13/149313-050-E9E1EC08/SR-Crown-Hall-Illinois-Institute-of-Technology-1956.jpg',
     },
     {
-      title: 'College of Dupage',
+      title:
+        'Illinois Institute of Technology - Information Technology Management',
+      caption: "Bachelor's Degree",
+      imageUrl:
+        'https://www.cod.edu/news/images/campus/src-exterior-article.jpg',
+    },
+    {
+      title: 'College of Dupage - Computer Science',
       caption: "Associate's Degree",
       imageUrl:
         'https://www.cod.edu/news/images/campus/src-exterior-article.jpg',
@@ -21,6 +28,7 @@ const Home = () => {
   return (
     <div className="container">
       <Banner />
+      <div className="education-text">Education</div>
       {cardsData.map((card, index) => (
         <Card
           key={index}
